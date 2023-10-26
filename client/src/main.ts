60ee8239529a817a0b0674736ebe2566050b0200
nana. <44186742+nana-boateng@users.noreply.github.com>
2023-10-26 11:56:09 -0400

    Refractor.
    I upgraded the current app to Angular 16, but for some reason the
    compilation was weird and I found it hard to debug errors. I have
    figured out the general flow of the app, and so I believe I can rebuild
    it much faster than I can figuring out weird edge cases.

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,7 +2,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .catch((err) => console.error(err));
