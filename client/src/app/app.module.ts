14617ffa31edd5758c13fd7d836df7a4236abda9
nana. <44186742+nana-boateng@users.noreply.github.com>
2023-10-26 11:56:09 -0400

    Refractor.
    I upgraded the current app to Angular 16, but for some reason the
    compilation was weird and I found it hard to debug errors. I have
    figured out the general flow of the app, and so I believe I can rebuild
    it much faster than I can figuring out weird edge cases.

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    StoreModule.forRoot({}, {})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
