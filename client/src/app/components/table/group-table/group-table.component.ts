4712df7e8cd664539f6da658187ebaa44a084fa1
Nana Boateng <n.boateng@utoronto.ca>
2023-11-06 17:04:21 -0500

    Group selection done

---
+++ client/src/app/components/table/group-table/group-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-group-table',
+  templateUrl: './group-table.component.html',
+  styleUrls: ['./group-table.component.css']
+})
+export class GroupTableComponent {
+
+}
