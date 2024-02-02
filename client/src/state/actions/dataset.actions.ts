2e3abe659ef51d0e0046e1978071a0e1cb0aa3f3
Nana Boateng <n.boateng@utoronto.ca>
2024-02-02 13:37:39 -0500

    maintenance(genera;): remove unused folder

--- client/src/state/actions/dataset.actions.ts
+++
@@ -1,4 +0,0 @@
-import { createAction, props  } from "@ngrx/store";
-import { VariableGroups } from "../interface";
-
-export const datasetVariableGroupsLoaded = createAction('[Dataset Variable] Groups Loaded', props<{ variableGroups: VariableGroups }>())
