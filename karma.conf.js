13466329a6d7abb5853739819e5c190681a44a02
nana boateng <n.boateng@utoronto.ca>
2024-04-25 10:18:32 -0400

    Fix testing (THIS IS NOT BEST)

--- karma.conf.js
+++ karma.conf.js
@@ -14,8 +14,8 @@ module.exports = function(config) {
     ],
     client: {
       jasmine: {
-        // you can add configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+        // you can add configuration options-button for Jasmine here
+        // the possible options-button are listed at https://jasmine.github.io/api/edge/Configuration.html
         // for example, you can disable the random execution with `random: false`
         // or set a specific seed with `seed: 4321`
         random: false
