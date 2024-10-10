afa40ca77c2e16c6d936cd841a6e4850f81473b0
nana boateng <n.boateng@utoronto.ca>
2024-10-10 12:43:50 -0400

    Add table store

---
+++ src/app/new.state/table/table.store.ts
@@ -0,0 +1,31 @@
+// import { VariablesSimplified } from "../xml/xml.interface";
+// import {signalStore} from '@ngrx/signals'
+//
+// type TableState = {
+//   variables: VariablesSimplified[];
+//   filter: {
+//     orderBy: 'variableID' | 'name' | 'label' | 'weight' | ''
+//     order: 'asc' | 'desc'
+//     query: string;
+//   };
+// };
+//
+// const initialState: TableState = {
+//   variables: [],
+//   filter: {
+//     orderBy: '',
+//     order: 'asc',
+//     query: ''
+//   }
+// }
+//
+// export const TableStore = signalStore(
+//   withState(initialState),
+//   withMethod((store) => ({
+//     updateQuery(query: string):void {
+//
+//       }
+//     }),
+//
+// }))
+// )
