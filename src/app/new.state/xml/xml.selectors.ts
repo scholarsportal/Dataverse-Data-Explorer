334ee63edad6ee766ab6ade1e6d8da2ad8000b4a
nana boateng <n.boateng@utoronto.ca>
2024-12-03 15:42:44 -0500

    Hotfix, single variable error

--- src/app/new.state/xml/xml.selectors.ts
+++ src/app/new.state/xml/xml.selectors.ts
@@ -62,7 +62,11 @@ export const selectDatasetCitation = createSelector(
 export const selectDatasetVariables = createSelector(
   selectXmlFeature,
   (state): Variable[] => {
-    return state.dataset?.codeBook.dataDscr?.var ?? [];
+    return Array.isArray(state.dataset?.codeBook.dataDscr.var)
+      ? state.dataset?.codeBook.dataDscr.var
+      : state.dataset?.codeBook.dataDscr.var
+        ? [state.dataset?.codeBook.dataDscr.var]
+        : [];
   },
 );
 
