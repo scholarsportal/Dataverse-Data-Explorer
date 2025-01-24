ef7a54f6de69578a78f77aa6d50c032e5fd77351
nana boateng <n.boateng@utoronto.ca>
2025-01-24 14:28:23 -0500

    Update Matomo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,4 +3,7 @@ export const environment = {
   domain: 'http://localhost:3489',
   matomoScriptUrl:
     'https://analytics.scholarsportal.info/js/container_JBxcbS7D.js',
+  matomoTrackerUrl: 'https://analytics.scholarsportal.info/matomo.php',
+  matomoSiteId: 128,
+  serverUrl: 'https://analytics.scholarsportal.info',
 };
