c0757416e347b9ac6921a62ac459f7a6ff71df27
nana boateng <n.boateng@utoronto.ca>
2025-04-22 17:02:34 -0400

    maintenance: remove NgRx DevTools from production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,8 @@
-import { provideStoreDevtools } from '@ngrx/store-devtools';
-
 export const environment = {
   production: true,
   domain: 'http://localhost:3489',
   serverUrl: 'https://analytics.scholarsportal.info',
-  providers: [
-    provideStoreDevtools({
-      maxAge: 25,
-      logOnly: true,
-      connectInZone: true,
-    }),
-  ],
+  providers: [],
   matomoTrackerUrl: '',
   matomoSiteId: '',
 };
