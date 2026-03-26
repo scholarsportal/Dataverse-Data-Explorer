157ab7e17953d88318618d4df674e2b3ac55d2c9
nana boateng <n.boateng@utoronto.ca>
2026-03-26 17:53:01 -0400

    init: project setup and foundation

--- apps/data-explorer/src/app/app.config.ts
+++ apps/data-explorer/src/app/app.config.ts
@@ -4,14 +4,9 @@ import {
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
-import {
-  provideClientHydration,
-  withEventReplay,
-} from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideClientHydration(withEventReplay()),
     provideBrowserGlobalErrorListeners(),
     provideRouter(appRoutes),
   ],
--- libs/shared/models/src/lib/models/models.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'dde-models',
-  imports: [],
-  templateUrl: './models.html',
-  styleUrl: './models.css',
-})
-export class Models {}
